bb314a83697529ba41aa5400c559b44d1287bbb2
Wickedlolz <iceshotgg.linux@gmail.com>
2022-03-28 19:16:03 +0300

    removing 404.html

--- src/app.js
+++ src/app.js
@@ -9,13 +9,6 @@ import { loadMovie } from './middlewares/loadMovie.js';
 import { editPage } from './pages/edit.js';
 import { profilePage } from './pages/profile.js';
 
-let path = localStorage.getItem('path');
-if (path) {
-    localStorage.removeItem('path');
-
-    page.redirect(path);
-}
-
 page(decorateContext);
 page('/', catalogPage);
 page('/login', loginPage);
